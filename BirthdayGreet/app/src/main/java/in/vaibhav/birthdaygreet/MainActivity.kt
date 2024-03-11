45a949e2e1c3da3388343d162ff8b8ac91891873
Vaibhav Patel <vaibhavpatel003@gmail.com>
2024-03-12 02:46:30 +0530

    Added Basic Birthday Greet App

---
+++ BirthdayGreet/app/src/main/java/in/vaibhav/birthdaygreet/MainActivity.kt
@@ -0,0 +1,79 @@
+package `in`.vaibhav.birthdaygreet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import `in`.vaibhav.birthdaygreet.ui.theme.BirthdayGreetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayGreetTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday!",
+                        from = "From Vaibhav",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+
+    ) {
+        Text(
+            text = message,
+            fontSize = 60.sp,
+            lineHeight = 116.sp,
+            modifier = Modifier.padding(vertical = 40.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+
+        )
+    }
+}
+
+
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    BirthdayGreetTheme {
+        GreetingText(message = "Happy Birthday!", from ="From Vaibhav")
+    }
+}
