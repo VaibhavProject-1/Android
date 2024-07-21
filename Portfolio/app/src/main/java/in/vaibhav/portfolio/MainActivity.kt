dae42e906f931b1badb08c74efacf727a07b0e35
Vaibhav Patel <vaibhavpatel003@gmail.com>
2024-07-22 00:30:49 +0530

    Finished Portfolio App

--- Portfolio/app/src/main/java/in/vaibhav/portfolio/MainActivity.kt
+++ Portfolio/app/src/main/java/in/vaibhav/portfolio/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,16 +14,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +40,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import `in`.vaibhav.portfolio.ui.theme.PortfolioTheme
+import `in`.vaibhav.portfolio.ui.theme.Purple40
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,8 +54,48 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun getProjectList():List<Project>{
+    return listOf(
+        Project(name = "TuneSync", desc = "Spotify Clone with payment integration"),
+        Project(name = "Todo", desc = "Todo using Redux and theme"),
+    )
+}
+
+data class Project(
+    val name: String,
+    val desc: String,
+)
+
+
+@Composable
+fun ProjectItem(project: Project) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        Image(painter = painterResource(id = R.drawable.profile), contentDescription = "",
+            Modifier
+                .size(60.dp)
+                .clip(
+                    CircleShape
+                ))
+        Column (Modifier.padding(horizontal = 8.dp)){
+            Text(text = project.name, style = MaterialTheme.typography.labelLarge)
+            Spacer(
+                Modifier
+                    .padding(top = 2.dp)
+            )
+            Text(text = project.desc, style = MaterialTheme.typography.bodySmall)
+        }
+    }
+}
+
 @Composable
 fun Portfolio(modifier: Modifier = Modifier) {
+
+    val isOpen = remember {
+        mutableStateOf(false)
+    }
+
     Surface(
         shadowElevation = 8.dp,
         shape = RoundedCornerShape(12.dp),
@@ -73,12 +124,46 @@ fun Portfolio(modifier: Modifier = Modifier) {
             Spacer(
                 Modifier
                     .fillMaxWidth()
+                    .padding(5.dp)
+            )
+            Row {
+                Image(painter = painterResource(id = R.drawable.instagram), contentDescription = "", modifier = Modifier
+                    .size(18.dp)
+                    .padding(end = 5.dp, bottom = 5.dp))
+                Text(text = "/vaibhav7407", style = MaterialTheme.typography.bodySmall)
+            }
+            Spacer(
+                Modifier
+                    .fillMaxWidth()
                     .padding(2.dp)
             )
             Row {
-                Image(painter = painterResource(id = R.drawable.instagram), contentDescription = "", modifier = Modifier.size(18.dp).padding(end = 5.dp))
-                Text(text = "/vaibhav7407", style = MaterialTheme.typography.bodySmall)
+                Image(painter = painterResource(id = R.drawable.github), contentDescription = "", modifier = Modifier
+                    .size(18.dp)
+                    .padding(end = 5.dp, top = 5.dp))
+                Text(text = "/VaibhavProject-1", style = MaterialTheme.typography.bodySmall)
             }
+            Spacer(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            Button(onClick = { isOpen.value = !isOpen.value }, colors = ButtonDefaults.buttonColors(Purple40)) {
+                Text(text = "My Projects")
+            }
+            Spacer(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            if(isOpen.value){
+                LazyColumn(modifier = Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                    items(getProjectList()){
+                        ProjectItem(it)
+                    }
+                }
+            }
+
         }
         }
 
